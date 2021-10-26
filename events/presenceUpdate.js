d4eab3480cd1bffd7efea162a1ef4ce2888ab226
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2021-10-26 10:06:29 +0800

    make the bot's activity sync with nemu's

---
+++ events/presenceUpdate.js
@@ -0,0 +1,15 @@
+module.exports = (client, oldPresence, newPresence) => {
+  // Nemu's id = '753150492380495894'
+  // My id     = '545427431662682112'
+
+  if (newPresence.userId !== '753150492380495894')
+      return;
+
+  const { name, type, url } = newPresence.activities.filter(x => x.type !== 'CUSTOM')[0] || {};
+
+  if (name && type){
+      return client.user.setActivity(name, { type, url });
+  } else {
+      return console.log(client.user.setActivity('Nemuphobia', { type: 'PLAYING' }));
+  };
+};
