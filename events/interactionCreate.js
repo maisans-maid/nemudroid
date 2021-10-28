8a6a4569ab41d109773658cd73a0fd294ea73cb1
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2021-10-28 18:17:20 +0800

    fix(handler): Better interaction handling
    * Send message to user instead of logging the error.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,9 @@
 module.exports = (client, interaction) => {
-  try {
-    if (!interaction.isCommand()) return;
 
-    const exefunc = client.commands.get(interaction.commandName);
+  if (!interaction.isCommand()) return;
 
-    exefunc(client, interaction);
-  } catch (e) {
-    console.log('\x1b[33m[!]\x1b[0m ' + e.message);
-  };
+  const exefunc = client.commands.get(interaction.commandName);
+  if (!exefunc) return interaction.reply({ content: `${interaction.commandName} has no or has missing command module.`, ephemeral: true });
+  exefunc(client, interaction);
+
 };
