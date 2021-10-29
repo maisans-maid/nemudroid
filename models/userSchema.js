cf849e2b94b6acb8d59a2a13c76328d5b99b6340
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2021-10-29 21:20:14 +0800

    update(command): updated (/) level ui
    * Usage of canvas module to create image for ranked cards
    * Addition of new font-profile (Aller Bold)
    * Addition of rank information

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,6 +2,7 @@ const { model, Schema } = require('mongoose');
 
 module.exports = model('user_profiles', Schema({
   _id: String,
+  wallpaper: {type: String, default: null },
   xp: { type: Array, default: []}
 }, {
   versionKey: false
