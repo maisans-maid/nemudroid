acea45c57199b7e72f134737016dbe530f0aa7c2
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2021-11-02 21:07:35 +0800

    📦 refactor(command): achievement command

--- commands/achievement.js
+++ commands/achievement.js
@@ -2,79 +2,95 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { createCanvas, loadImage } = require('node-canvas');
 const { join } = require('path');
 
-// To do before launch
-// 1. Ask permission from nemu to use her minecraft skin as the icon
-// 2. Manage permissions (do we allow all users to use this command or just some people with certain roles)
-
 const command = new SlashCommandBuilder()
 .setName('achievement')
 .setDescription('Generate your own Minecraft Achievement.')
 .addStringOption(option => option
-  .setName('description')
-  .setDescription('Description of the achievement. Max 25 characters.')
-  .setRequired(true)
+    .setName('description')
+    .setDescription('Description of the achievement. Max 25 characters.')
+    .setRequired(true)
 )
 .addStringOption(option => option
-  .setName('title')
-  .setDescription('Title of the achievement. Max 25 characters.')
+    .setName('title')
+    .setDescription('Title of the achievement. Max 25 characters.')
 )
 .addUserOption(option => option
-  .setName('target')
-  .setDescription('The user who achieved this achievement')
+    .setName('target')
+    .setDescription('The user who achieved this achievement')
 );
 
+
 module.exports = {
-  builder: command,
-  execute: async (client, interaction) => {
+    builder: command,
+    execute: async (client, interaction) => {
 
-    const title = (interaction.options.getString('title') || 'Achievement Get!').substr(0,25).replace(/\s/g, '  ');
-    const descr = interaction.options.getString('description').substr(0,25).replace(/\s/g, '  ');
-    const target = interaction.options.getUser('target');
-    /**NOTE REGARDING THE REPLACE FN: Minecraftia font's "space width" is less than Minecraft's so we have to double them up*/
-    const title_rgb = 'rgb(255,255,0)';    // Yellow
-    const descr_rgb = 'rgb(255,255,255)';  // White
+        let title = interaction.options.getString('title');
+        let descr = interaction.options.getString('description');
+        const target = interaction.options.getUser('target');
 
-    // Constructor
-    const bglength = getBGLength(descr,title)
-    const side = await loadImage(join(__dirname, '..', 'assets', 'images', 'minecraft', 'minecraft_achievement_side.png'));
-    const middle = await loadImage(join(__dirname, '..', 'assets', 'images', 'minecraft', 'minecraft_achievement_middle.png'));
-    const nemu = await loadImage(join(__dirname, '..', 'assets', 'images', 'minecraft', 'nemu_minecraft_icon.png'));
-    const canvas = createCanvas(bglength + 24 /*12 pixels each sides for side constructor*/, 64);
-    const ctx = canvas.getContext('2d');
+        if (!title)
+            title = 'Achievement Get!';
 
-    ctx.drawImage(side,0,0); // Origin set at coordinates 0,0
-    ctx.drawImage(middle,12,0, bglength, 64); // Stretch dat image
+        if (title.length > 25)
+            title = title.substr(0, 25);
 
-    ctx.save();
-    ctx.rotate(Math.PI); // Rotate 180 deg
-    ctx.drawImage(side,-canvas.width,-canvas.height); // Place the next side on the other end of the canvas
-    ctx.restore();
-    ctx.drawImage(nemu, 16, 16);
+        if (description.length > 25)
+            descr = descr.substr(0, 25);
 
-    ctx.font = '16px Minecraftia, "Code2003", "Unifont"';
-    ctx.fillStyle = 'rgb(255,255,0)';
-    ctx.fillText(title, 60, 38, bglength);
-    ctx.fillStyle = 'rgb(255,255,255)';
-    ctx.fillText(descr, 60, 60, bglength);
+        descr = descr.replace(/\s/g, '  ');
 
-    const success = `Successfully generated Minecraft Achievement: **${title}!**`;
-    const fail = (e) => `Unable to generate Minecraft Achievement: ${e.message}`;
+        const title_rgb = 'rgb(255,255,0)';
+        const descr_rgb = 'rgb(255,255,255)';
 
-    return interaction.channel.send({ content: target ? `${target}, You got an achievement!` : 'Achievement Get!' , files: [{
-      attachment: canvas.toBuffer(),
-      name: `achievement_nemudroid.png`
-    }]})
-    .then(() =>  interaction.reply({ content: success, ephemeral: true }))
-    .catch(e => interaction.reply({ content: fail(e), ephemeral: true }));
-  }
+        const __minecraft = '../assets/images/minecraft/';
+
+        const bglength = getBGLength(descr, title);
+        const size     = await loadImage(join(__dirname, __minecraft, 'minecraft_achievement_side.png'));
+        const middle   = await loadImage(join(__dirname, __minecraft, 'minecraft_achievement_middle.png'));
+        const nemu     = await loadImage(join(__dirname, __minecraft, 'nemu_minecraft_icon.png'));
+
+        const canvas = createCanvas(bglength + 24, 64);
+        const ctx    = canvas.getContext('2d');
+
+        ctx.drawImage(side, 0, 0);
+        ctx.drawImage(middle, 12, 0, bglength, 64);
+
+        ctx.save();
+        ctx.rotate(Math.PI);
+        ctx.drawImage(side, -canvas.width, -canvas.height);
+        ctx.restore();
+        ctx.drawImage(nemu, 16, 16);
+
+        ctx.font = '16px Minecraftia';
+        ctx.fillStyle = title_rgb;
+        ctx.fillText(title, 60, 38, bglength);
+        ctx.fillStyle = descr_rgb;
+        ctx.fillText(descr, 60, 60, bglength);
+
+        return interaction.channel.send({
+            content: target
+                ? `${target}, You got an achievement!`
+                : 'Achievement Get!',
+            files: [{
+                attachment: canvas.toBuffer(),
+                name: 'achievement_nemudroid.png'
+            }]
+        }).then(() => interaction.reply({
+            content: `Successfully generated Minecraft Achievement: **${title}**`,
+            ephemeral: true
+        })).catch(e => interaction.reply({
+            content: `Unable to generate Minecraft Achievement: ${e.message}`,
+            ephemeral: true
+        }));
+    }
 };
 
 function getBGLength(descr, title){
-  const canvas = createCanvas(100,14);
-  const ctx = canvas.getContext('2d');
-  ctx.font = '12px Minecraftia, "Code2003", "Unifont"';
-  const dwidth = ctx.measureText(descr).width;
-  const twidth = ctx.measureText(title).width;
-  const width  = dwidth > twidth ? dwidth : twidth; // If dwidth is greater than twidth, use dwith, otherwise, use twidth;
-  return (width < 250 ? 250 : width) + 54; // add 54 to give space to the icon w/c is 54x54
+    const canvas = createCanvas(100,14);
+    const ctx = canvas.getContext('2d');
+    ctx.font = '16px Minecraftia, "Code2003", "Unifont"';
+    const dwidth = ctx.measureText(descr).width;
+    const twidth = ctx.measureText(title).width;
+    const width  = dwidth > twidth ? dwidth : twidth; // If dwidth is greater than twidth, use dwith, otherwise, use twidth;
+    return (width < 250 ? 250 : width) + 54; // add 54 to give space to the icon w/c is 54x54
 };
