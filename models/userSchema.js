f37acdd4f3e3539c44fdb72bff4c26e743487bea
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2021-11-02 21:08:59 +0800

    ✨ feat: currency

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,9 +1,27 @@
 const { model, Schema } = require('mongoose');
 
 module.exports = model('userProfile', Schema({
-  _id: String,
-  wallpaper: {type: String, default: null },
-  xp: { type: Array, default: []}
+    _id: String,
+    wallpaper: {
+        type: String,
+        default: null
+    },
+    xp: {
+        type: Array,
+        default: []
+    },
+    xpMultipliers: {
+        type: Array,
+        default: []
+    },
+    inventory: {
+        type: Array,
+        default: []
+    },
+    credits: {
+        type: Number,
+        default: 0
+    }
 }, {
   versionKey: false
 }));
