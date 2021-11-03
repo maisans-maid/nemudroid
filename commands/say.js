2fe36bcfe00ceef4e47b975d2db64cce0d60954f
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2021-11-04 01:49:44 +0800

    📦 refactor(command): say command

--- commands/say.js
+++ commands/say.js
@@ -5,38 +5,51 @@ const command = new SlashCommandBuilder()
 .setName('say')
 .setDescription('Say something using the bot')
 .addStringOption(option => option
-  .setName('content')
-  .setDescription('The content of the message to be sent.')
-  .setRequired(true)
+    .setName('content')
+    .setDescription('The content of the message to be sent.')
+    .setRequired(true)
 )
 .addChannelOption(option => option
-  .setName('channel')
-  .setDescription('The channel you want the message to be sent. Defaults to the current channel.')
+    .setName('channel')
+    .setDescription('The channel you want the message to be sent. Defaults to the current channel.')
 )
 
+const permalink = 'https://discord.js.org/#/docs/main/stable/typedef/TextBasedChannels';
 
 module.exports = {
-  builder: command,
-  execute: async (client, interaction) => {
+    builder: command,
+    execute: async (client, interaction) => {
 
-    if (!interaction.memberPermissions.has(FLAGS.MANAGE_GUILD)){
-      return interaction.reply({ content: `You are not allowed to use this command!`, ephemeral: true });
-    };
+        if (!interaction.memberPermissions.has(FLAGS.MANAGE_GUILD))
+            return interaction.reply({
+                ephemeral: true,
+                content: '❌ You are not allowed to use this command!'
+            });
 
-    const content = interaction.options.getString('content');
-    const channel = interaction.options.getChannel('channel');
+        const content = interaction.options.getString('content');
 
-    if (channel){
-      if (!channel.isText()){
-        return interaction.reply({ ephemeral: true, content: `\\❌ The selected channel is not a [**\`Text-based Channel\`**](${hideLinkEmbed('https://discord.js.org/#/docs/main/stable/typedef/TextBasedChannels')})`})
-      };
-      return channel.send(content)
-      .then(() => interaction.reply({ content: '\\✔️ Message has been delivered!', ephemeral: true }))
-      .catch(e => interaction.reply({ content: `\\❌ ${e.message}`, ephemeral: true }));
-    } else {
-      return interaction.channel.send(content)
-      .then(() => interaction.reply({ content: '\\✔️ Message has been delivered!', ephemeral: true }))
-      .catch(e => interaction.reply({ content: `\\❌ ${e.message}`, ephemeral: true }));
-    };
-  }
+        const channel = interaction.options
+            .getChannel('channel') ||
+            interaction.channel;
+
+        if (!channel.isText())
+            return interaction.reply({
+                ephemeral: true,
+                content: `❌ The selected channel is not a [**\`Text-based Channel\`**](${hideLinkEmbed(permalink)})`
+            });
+
+        return channel.send(content)
+            .then(() => interaction
+                .reply({
+                    ephemeral: true,
+                    content: '✔️ Message has been delivered!'
+                })
+            )
+            .catch(error => interaction
+                .reply({
+                    ephemeral: true,
+                    content: `❌ Error: ${error.message}`
+                })
+            );
+    }
 };
