29c80a1bed83d6e3a03db4c139bf6228399ac59f
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2021-11-04 01:38:22 +0800

    📦 refactor(command): reload command

--- commands/reload.js
+++ commands/reload.js
@@ -5,34 +5,57 @@ const command = new SlashCommandBuilder()
 .setName('reload')
 .setDescription('Reload a specific command module')
 .addStringOption(option => option
-  .setName('command')
-  .setDescription('The name of the command to reload')
-  .setRequired(true)
+    .setName('command')
+    .setDescription('The name of the command to reload')
+    .setRequired(true)
 )
 
 module.exports = {
-  builder: command,
-  execute: async (client, interaction) => {
+    builder: command,
+    execute: async (client, interaction) => {
 
-    if (interaction.member.id !== '545427431662682112'){
-      return interaction.reply({ content: `You are not allowed to use this command! Contact my developer if you wish to have access.`, ephemeral: true });
-    };
+        if (interaction.member.id !== '545427431662682112')
+            return interaction.reply({
+                ephemeral: true,
+                content: '❌ You are not allowed to use this command!'
+            });
 
-    const commandName = interaction.options.getString('command');
+        const commandName = interaction.options
+            .getString('command');
 
-    if (!client.commands.get(commandName.toLowerCase())){
-      return interaction.reply({ content: `Command Module for **${commandName}** could not be found.`, ephemeral: true });
-    };
+        if (!client.commands.get(commandName.toLowerCase()))
+            return interaction.reply({
+                ephemeral: true,
+                content: `❌ Command Module for **${commandName}** could not be found.`
+            });
 
-    try {
-     delete require.cache[require.resolve(join(__dirname, commandName))];
-     const cmdModule = require(join(__dirname, commandName));
-     client.commands.set(cmdModule.builder.name, cmdModule.execute);
+        try {
+            delete require.cache[
+                require.resolve(join(
+                    __dirname,
+                    commandName
+                ))
+            ];
 
-     return interaction.reply({ content: `Command **${commandName}** has been reloaded.`, ephemeral: true });
+            const cmdModule = require(join(
+                __dirname,
+                commandName
+            ));
 
-   } catch (err) {
-     return interaction.reply({ content: err.message, ephemeral: true });
-   };
-  }
+            client.commands.set(
+                cmdModule.builder.name,
+                cmdModule.execute
+            );
+
+            return interaction.reply({
+                ephemeral: true,
+                content: `Command **${commandName}** has been reloaded.`
+            });
+        } catch (err) {
+            return interaction.reply({
+                ephemeral: true,
+                content: `❌ ${err.message}`
+            });
+        };
+    }
 };
