fc7276bc45629ecf96b6cdeec0b83005b169ecdf
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2022-05-13 12:56:15 +0800

    📦 chore(presence): change activity

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -4,13 +4,13 @@ const productionId = '753150492380495894';
 const developerId  = '545427431662682112';
 
 module.exports = (client, oldPresence, newPresence) => {
-    if (newPresence.userId !== developerId) return;
+    if (newPresence.userId !== productionId) return;
 
     const { name, type, url, details } = newPresence.activities.filter(x => x.type !== 'CUSTOM')[0] || {};
 
     if (name && type){
         return client.user.setActivity(details || name, { type, url });
     } else {
-        return client.user.setActivity('Nemuphobia', { type: 'PLAYING' });
+        return client.user.setActivity(`${client.users.cache.size} active users`, { type: 'LISTENING' });
     };
 };
