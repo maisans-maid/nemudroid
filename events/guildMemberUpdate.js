4d21c1511176e96ee668f6c261c5c7cca6c65f82
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2022-05-13 12:53:03 +0800

    📦 chore: remove memberupdate fns

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,15 +1,15 @@
 'use strict';
 
 module.exports = (client, newMem, oldMem) => {
-    if (newMem.guild.id !== '874162813977919488') return;
-
-    // Verify role was added
-    if ((oldMem.roles.cache.size === 0) && (newMem.roles.cache.size === 1)){
-      // Add border roles to member
-        newMem.roles.add([
-            '918501515445026916',
-            '918501825630580846',
-            '919534149352714270'
-        ]).catch(console.error);
-    };
+    // if (newMem.guild.id !== '874162813977919488') return;
+    //
+    // // Verify role was added
+    // if ((oldMem.roles.cache.size === 0) && (newMem.roles.cache.size === 1)){
+    //   // Add border roles to member
+    //     newMem.roles.add([
+    //         '918501515445026916',
+    //         '918501825630580846',
+    //         '919534149352714270'
+    //     ]).catch(console.error);
+    // };
 }
