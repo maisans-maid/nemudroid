645e18a35b251e8294879ac892557de648f2617e
Maisans-maid <56829176+maisans-maid@users.noreply.github.com>
2022-05-15 17:38:01 +0800

    🐛 fix: locked games not releasing the playing status of the user

--- games/rps.js
+++ games/rps.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = async interaction => {
+    interaction.client.custom.cache.games.get(interaction.guildId).delete(interaction.user.id)
     return interaction.reply({
         ephemeral: true,
         content: '🔒 This feature is currently locked'
